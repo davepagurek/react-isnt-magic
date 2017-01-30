ac8640491600107bf221cee15127c52354ee4505
Dave Pagurek <dave@davepagurek.com>
2017-01-30 10:57:49 -0500

    Add location history, more slides

--- slides.js
+++ slides.js
@@ -12,5 +12,39 @@ window.addEventListener("keydown", function(event) {
   if (nextSlide) {
     current.classList.remove("visible");
     nextSlide.classList.add("visible");
+
+    window.history.pushState({slide: nextSlide.id}, "React Isn't Magic", "#" + nextSlide.id);
+
+    //[].forEach.call(nextSlide.querySelectorAll('iframe'), function (iframe) {
+      //console.log(iframe);
+      //try {
+        //iframe.contentWindow.postMessage('jsbin:refresh', '*');
+      //} catch (e) { console.error(e); }
+    //});
   }
 });
+
+window.addEventListener("popstate", function(event) {
+  document.querySelector(".visible").classList.remove("visible");
+  goToSlide(event.state ? event.state.slide : window.location.hash.slice(1));
+});
+
+function goToSlide(slideNum) {
+  var currentSlide = document.getElementById(slideNum);
+  currentSlide = currentSlide || document.querySelector(".slide");
+  currentSlide.classList.add("visible");
+}
+
+
+console.log(document.querySelectorAll(".slide"));
+Array.prototype.forEach.call(
+  document.querySelectorAll(".slide"),
+  function(slide, i) {
+    slide.id = "" + (i+1);
+  }
+);
+
+if (window.location.hash) {
+  goToSlide(window.location.hash.slice(1));
+}
+
